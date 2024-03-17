f520e704cc4183e851f25940ed8fb5af3cd635f1
Silvia Saverino <silviasave89@gmail.com>
2024-03-17 18:04:56 +0000

    add js file with meme data

--- src/assets/components/Meme.jsx
+++ src/assets/components/Meme.jsx
@@ -1,16 +1,19 @@
-import React from "react"
-
-
 export default function Meme() {
     return (
         <main>
             <form action="">
-                <input
-                    type="text"
-                    placeholder="Text here" />
-                <input
-                    type="text"
-                    placeholder="Text here" />
+                <div className="form-inputs">
+                    <label>Top text:</label>
+                    <input
+                        type="text"
+                        placeholder="Text here" />
+                </div>
+                <div className="form-inputs">
+                    <label>Bottom text:</label>
+                    <input
+                        type="text"
+                        placeholder="Text here" />
+                </div>
                 <button className="form-btn">Get a new meme</button>
             </form>
         </main>
