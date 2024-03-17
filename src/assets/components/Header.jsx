f9d152f024e16b0a86019088c67f5c8a864eab59
Silvia Saverino <silviasave89@gmail.com>
2024-03-17 17:10:09 +0000

    add header and style

---
+++ src/assets/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Header(){
+    return (
+        <header>
+            <img className="logo" src="../public/images/logo.png" alt="meme generator logo" />
+            <h4 className="header-title">Meme Generator project</h4>
+        </header>
+    )
+}
